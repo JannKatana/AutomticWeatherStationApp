ff9a804adaa21c95a308e9f363aa3e4a1581b95d
Mel-john Catanaoan <mjtcatanaoan@gmail.com>
2019-08-26 21:39:29 +0800

    Updated .gitignore and added db models

--- aws_web_app/aws_core/admin.py
+++ aws_web_app/aws_core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from aws_core.models import Station_Type, Station_Data, Station
 
 # Register your models here.
+admin.site.register(Station_Type)
+admin.site.register(Station_Data)
+admin.site.register(Station)
--- aws_web_app/aws_core/models.py
+++ aws_web_app/aws_core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+phone_regex = RegexValidator(regex=r'^\+?1?\d{12}$', message="Incorrect format. Please try again.")
+
+class Station_Type(models.Model):
+    type_name = models.CharField(max_length=16, unique=True)
+
+    def __str__(self):
+        return self.type_name
+
+
+class Station(models.Model):
+    station_name = models.CharField(max_length=64, unique=True)
+    station_type = models.OneToOneField(Station_Type, on_delete=models.CASCADE)
+    station_num  = models.CharField(validators=[phone_regex], max_length=13, blank=True)
+    location_lat = models.FloatField()
+    loaction_lon = models.FloatField()
+    column_names = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.station_name
+
+
+class Station_Data(models.Model):
+    data_row     = models.CharField(max_length=256)
+    station      = models.ForeignKey(Station, on_delete=models.CASCADE)
